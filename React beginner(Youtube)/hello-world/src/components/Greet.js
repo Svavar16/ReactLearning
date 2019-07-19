6791e2e729ada7b44956ecb8fa05f6de8a1c92a9
svavar16 <spg009@gmail.com>
2019-07-19 14:24:29 +0000

    adding parameter

--- React beginner(Youtube)/hello-world/src/components/Greet.js
+++ React beginner(Youtube)/hello-world/src/components/Greet.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
-const Greet = () => <h1>Hello Svavar</h1>
+const Greet = (props) => {
+console.log(props)
+return <h1>Hello { props.name }</h1>
+}
 
 export default Greet
