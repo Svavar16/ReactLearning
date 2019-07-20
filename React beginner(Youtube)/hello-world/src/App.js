a5a8cc217497221c2fa37ec8e380ff618f038da7
svavar16 <spg009@gmail.com>
2019-07-20 12:22:39 +0000

    click functions

--- React beginner(Youtube)/hello-world/src/App.js
+++ React beginner(Youtube)/hello-world/src/App.js
@@ -4,23 +4,27 @@ import './App.css';
 import Counter from './components/Counter'
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
-{/*import Hello from './components/Hello'
-import Message from './components/message'*/}
+import Hello from './components/Hello'
+import Message from './components/message'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 
 function App() {
   return (
     <div className="App">
+      <ClassClick></ClassClick>
+      {/*<FunctionClick></FunctionClick>
       <Counter></Counter>
       <Greet name='Bruce' heroName='Batman'> 
       <p>This is a child paragraph</p>
       </Greet>
-      {/*<Greet name='Clark' heroName='Superman'>
+      <Greet name='Clark' heroName='Superman'>
         <button>Action</button>
       </Greet>
-  <Greet name='Diana' heroName='Wonder Woman'/>*/}
+  <Greet name='Diana' heroName='Wonder Woman'/>
       <Welcome name='Bruce' heroName='Batman'></Welcome>
-      {/*<Welcome name='Clark' heroName='Superman'></Welcome>
+      <Welcome name='Clark' heroName='Superman'></Welcome>
       <Welcome name='Diana' heroName='Wonder Woman'></Welcome>
       <Message></Message>*/}
     </div>
