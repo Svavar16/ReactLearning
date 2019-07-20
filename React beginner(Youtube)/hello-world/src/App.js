2d9c16669badf4a0c11594bfa3e6c86309263781
svavar16 <spg009@gmail.com>
2019-07-20 12:14:13 +0000

    destructiorin classes and functional

--- React beginner(Youtube)/hello-world/src/App.js
+++ React beginner(Youtube)/hello-world/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter'
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
-import Hello from './components/Hello'
-import Message from './components/message'
-import Counter from './components/Counter'
+{/*import Hello from './components/Hello'
+import Message from './components/message'*/}
+
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
-      {/*<Greet name='Bruce' heroName='Batman'> 
+      <Greet name='Bruce' heroName='Batman'> 
       <p>This is a child paragraph</p>
       </Greet>
-      <Greet name='Clark' heroName='Superman'>
+      {/*<Greet name='Clark' heroName='Superman'>
         <button>Action</button>
       </Greet>
-      <Greet name='Diana' heroName='Wonder Woman'/>
+  <Greet name='Diana' heroName='Wonder Woman'/>*/}
       <Welcome name='Bruce' heroName='Batman'></Welcome>
-      <Welcome name='Clark' heroName='Superman'></Welcome>
+      {/*<Welcome name='Clark' heroName='Superman'></Welcome>
       <Welcome name='Diana' heroName='Wonder Woman'></Welcome>
       <Message></Message>*/}
     </div>
