3b8fbb4e20cf963d8645d661b7c0c1a3b5ae69c0
svavar16 <spg009@gmail.com>
2019-09-17 12:15:23 +0000

    Fragments

--- React_beginner(Youtube)/hello-world/src/App.js
+++ React_beginner(Youtube)/hello-world/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Form from './components/Form';
 import LifecycleA from './components/LifycycleA';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Form />
-				<LifecycleA />
+				<Table />
 			</div>
 		);
 	}
