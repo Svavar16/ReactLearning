6431a8f96e3e73c93430f287c9756406bb735af2
svavar16 <spg009@gmail.com>
2019-09-23 13:40:19 +0000

    adding routes, route, switch and navbar

---
+++ hotel_reservasion_site_react/src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Error extends Component {
+    render() {
+        return (
+            <div>
+                hello from error page
+            </div>
+        );
+    }
+}
+
+export default Error;
