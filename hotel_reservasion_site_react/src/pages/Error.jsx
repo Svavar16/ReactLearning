f89412f612cb811d320c9b53a5b04873619bcd40
svavar16 <spg009@gmail.com>
2019-09-24 10:40:05 +0000

    Banner

--- hotel_reservasion_site_react/src/pages/Error.jsx
+++ hotel_reservasion_site_react/src/pages/Error.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from "react-router-dom";
 
 class Error extends Component {
     render() {
         return (
-            <Hero />
+            <Hero>
+                <Banner title="404" subtitle="Page not found">
+                    <Link to="" className="btn-primary">Return home</Link>
+                </Banner>
+            </Hero>
         );
     }
 }
